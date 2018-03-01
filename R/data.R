bc5707562eea26fe2a4b030fa158e1f15f95bc45
phgrosjean <phgrosjean@sciviews.org>
2018-03-01 15:53:30 +0100

    Do not use (yet) SciViews::R in examples

--- R/data.R
+++ R/data.R
@@ -69,7 +69,8 @@
 #' **all** the individuals still alive are measured (no sub-sampling).
 #'
 #' @examples
-#' SciViews::R
+#' #SciViews::R
+#' library(chart)
 #' chart(urchin_growth, formula = diameter ~ age) +
 #'   geom_jitter(alpha = 0.2) +
 #'   xlab(label(urchin_growth$age, units = TRUE)) +
