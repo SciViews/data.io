ff1072aca347cda0776e8c86525d8139733fc90a
phgrosjean <phgrosjean@sciviews.org>
2018-03-02 08:16:30 +0100

    Datasets objects are renamed from data_frame to dataframe, to avoir clash with the data_frame() function in tibble

--- tests/testthat/test-read.R
+++ tests/testthat/test-read.R
@@ -3,6 +3,6 @@ context("read")
 describe("read() R package datasets", {
 
   it("reads package datasets when package= argument is provided", {
-    expect_is(read("urchin_bio", package = "data"), "data_frame")
+    expect_is(read("urchin_bio", package = "data"), "dataframe")
   })
 })
