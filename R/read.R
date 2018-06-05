8f5f0dad8c6d3010e0a12a7e21ab459d5c96679f
phgrosjean <phgrosjean@sciviews.org>
2018-06-05 08:21:01 +0200

    Use of sidecar file for read() and examples added

--- R/read.R
+++ R/read.R
@@ -11,6 +11,11 @@
 #' @param comments Comments to add in the created object.
 #' @param package The package where to look for the dataset. If `file=` is not
 #'   provided, a list of available datasets in the package is displayed.
+#' @param sidecar_file If `TRUE` and a file with same name as `file=` + `.R` is
+#'   found in the same directory, it is considered as code to import these data
+#'   and it is sourced with `local = TRUE`, `chdir = TRUE` and
+#'   `verbose = FALSE`. That script **must** create an object named `dataset`,
+#'   which is the result that is returned by the function.
 #' @param fun The function to delegate reading of the data. If `NULL` (default),
 #'   The function is chosen from `fun_list`.
 #' @param fun_list The table with correspondance of the types, read, and write
@@ -30,10 +35,112 @@
 #' @keywords utilities
 #' @concept read and import data
 #' @examples
-#' # TODO...
-read <- function(file, type = NULL, header = "#", header.max = 50L, skip = 0L,
-  locale = default_locale(), comments = NULL, package = NULL, fun = NULL,
-  fun_list = NULL, ...) {
+#' # Use of read() as a more flexible substitute to data() (can change dataset
+#' # name and syntax more similar to read R datasets and datasets from files)
+#' read() # List all available datasets in your installed version of R
+#' # List datasets in one particular package
+#' read(package = "data")
+#' # Read one dataset from an R package, possibly changing its name
+#' (urchin <- read("urchin_bio", package = "data"))
+#'
+#' # Many example data files in the /extdata subdirectory of the package
+#' data_example <- function(path)
+#'   system.file("extdata", path, package = "data", mustWork = TRUE)
+#'
+#' # Various versions of the famous iris dataset
+#' (iris <- read(data_example("iris.csv")))
+#' (iris <- read(data_example("iris.csv.zip")))
+#' (iris <- read(data_example("iris.csv.tar"))) ##
+#' (iris <- read(data_example("iris.csv.tar.gz"))) ##
+#' (iris <- read(data_example("iris.csv.gz")))
+#' (iris <- read(data_example("iris.csv.bz2")))
+#' #(iris <- read(data_example("iris.csv.7z"))) ##
+#' (iris <- read(data_example("iris.tsv")))
+#' (iris <- read(data_example("iris.xls")))
+#' (iris <- read(data_example("iris.xlsx")))
+#' (iris <- read(data_example("iris.rds"))) # Does not tranform into tibble!
+#' #(iris <- read(data_example("iris.syd"))) ##
+#' (iris <- read(data_example("iris_short_header.csv"))) ##
+#' #(iris <- read(data_example("iris.csvy"))) ##
+#' #(iris <- read(data_example("iris.csvy.zip"))) ##
+#'
+#' # Require the feather package
+#' (iris <- read(data_example("iris.feather")))
+#'
+#' # Challenging datasets from the readr package
+#' library(readr)
+#' (mtcars <- read(readr_example("mtcars.csv")))
+#' (mtcars <- read(readr_example("mtcars.csv.zip")))
+#' (mtcars <- read(readr_example("mtcars.csv.bz2")))
+#' (challenge <- read(readr_example("challenge.csv"), guess_max = 1001))
+#' (massey <- read(readr_example("massey-rating.txt")))
+#' # By default, the type cannot be guessed from the extension
+#' # This is a space-separated vaules file (ssv)
+#' (massey <- read(readr_example("massey-rating.txt"), type = "ssv"))
+#' # or ...
+#' (massey <- read$ssv(readr_example("massey-rating.txt")))
+#' (epa <- read$ssv(readr_example("epa78.txt"), col_names = FALSE))
+#' (example_log <- read(readr_example("example.log")))
+#' # There are different ways to specify columns for fixed-width files (fwf)
+#' # See ?read_fwf in package readr
+#' (fwf_sample <- read$fwf(readr_example("fwf-sample.txt"),
+#'    col_positions =  fwf_cols(name = 20, state = 10, ssn = 12)))
+#'
+#' # Various examples of Excel datasets from readxl
+#' library(readxl)
+#' (xl <- read(readxl_example("datasets.xls")))
+#' (xl <- read(readxl_example("datasets.xlsx"), sheet = "mtcars"))
+#' (xl <- read(readxl_example("datasets.xlsx"), sheet = 3))
+#' # Accomodate a column with disparate types via col_type = "list"
+#' (clip <- read(readxl_example("clippy.xls"), col_types = c("text", "list")))
+#' (clip <- read(readxl_example("clippy.xlsx"), col_types = c("text", "list")))
+#' tibble::deframe(clip)
+#' # Read from a specific range in a sheet
+#' (xl <- read(readxl_example("datasets.xlsx"), range = "mtcars!B1:D5"))
+#' (deaths <- read(readxl_example("deaths.xls"), range = cell_rows(5:15)))
+#' (deaths <- read(readxl_example("deaths.xlsx"), range = cell_rows(5:15)))
+#' (type_me <- read(readxl_example("type-me.xls"), sheet = "logical_coercion",
+#'   col_types = c("logical", "text")))
+#' (type_me <- read(readxl_example("type-me.xlsx"), sheet = "numeric_coercion",
+#'   col_types = c("numeric", "text")))
+#' (type_me <- read(readxl_example("type-me.xls"), sheet = "date_coercion",
+#'   col_types = c("date", "text")))
+#' (type_me <- read(readxl_example("type-me.xlsx"), sheet = "text_coercion",
+#'   col_types = c("text", "text")))
+#' (xl <- read(readxl_example("geometry.xls"), col_names = FALSE))
+#' (xl <- read(readxl_example("geometry.xlsx"), range = cell_rows(4:8)))
+#'
+#' # Various examples from haven
+#' library(haven)
+#' haven_example <- function(path)
+#'   system.file("examples", path, package = "haven", mustWork = TRUE)
+#' (iris2 <- read(haven_example("iris.dta"))) # Stata v. 8-14
+#' (iris2 <- read(haven_example("iris.sav"))) # SPSS, TODO: labelled -> factor?
+#' (pbc <- read(data_example("pbc.por"))) # SPSS, POR format
+#' (iris2 <- read$sas(haven_example("iris.sas7bdat"))) # SAS file
+#' (afalfa <- read(data_example("afalfa.xpt"))) # SAS transport file
+read <- structure(function(file, type = NULL, header = "#", header.max = 50L,
+  skip = 0L, locale = default_locale(), comments = NULL, package = NULL,
+  sidecar_file = TRUE, fun = NULL, fun_list = NULL, ...) {
+  # Is there a sidecar file?
+  if (missing(file)) {
+    if (missing(package)) # No file or package provided: list all datasets
+      return(data())
+    file2 <- ""
+  } else {
+    file2 <- paste0(file, ".R")
+  }
+  if (isTRUE(sidecar_file) && file.exists(file2)) {
+    # Use a fake dataset content: it is supposed to be modified by the script
+    dataset <- NULL
+    source(file2, local = TRUE, chdir = TRUE, verbose = FALSE)
+    if (is.null(dataset))
+      stop("The script '", basename(file2),
+        "' did not produce a valid 'dataset' object.",
+        " Is it really a sidecar file?")
+    return(dataset)
+  }
+
   # Get fun_list from options() (and possibly install it)
   if (is.null(fun_list))
     fun_list <- getOption("read_write")
@@ -129,7 +236,7 @@ read <- function(file, type = NULL, header = "#", header.max = 50L, skip = 0L,
   } else {
     structure(fun(file, ...), comment = comments)
   }
-}
+}, class = c("subsettable_type", "function"))
 
 #' @export
 #' @rdname read
@@ -150,36 +257,56 @@ type_from_extension <- function(file) {
   NULL
 }
 
+#' @export
+#' @rdname read
+#' @param x A `subsettable_type` function.
+#' @param name The value to use for the `type=` argument.
+#' @method $ subsettable_type
+`$.subsettable_type` <- function(x, name)
+  function(...) x(type = name, ...)
+
+
+# Private functions -------------------------------------------------------
+
 .install_read_write_options <- function() {
   options(read_write = tibble::tribble(
-    ~type,     ~read_fun,            ~write_fun,
-    "csv",     "readr::read_csv",    "readr::write_csv",
-    "csv2",    "readr::read_csv2",   NA,
-    "tsv",     "readr::read_tsv",    "readr::write_tsv",
-    "csv.gz",  "readr::read_csv",     "readr::write_csv",
-    "csv2.gz", "readr::read_csv2",   NA,
-    "tsv.gz",  "readr::read_tsv",    "readr::write_tsv",
-    "csv.bz2", "readr::read_csv",    "readr::write_csv",
-    "csv2.bz2","readr::read_csv2",   NA,
-    "tsv.bz2", "readr::read_tsv",    "readr::write_tsv",
-    "csv.xz",  "readr::read_csv",    "readr::write_csv",
-    "csv2.xz", "readr::read_csv2",   NA,
-    "tsv.xz",  "readr::read_tsv",    "readr::write_tsv",
+    ~type,     ~read_fun,              ~write_fun,
+    "csv",     "readr::read_csv",      "readr::write_csv",
+    "csv2",    "readr::read_csv2",     NA,
+    "xlcsv",   "readr::read_csv",      "readr::write_excel_csv",
+    "tsv",     "readr::read_tsv",      "readr::write_tsv",
+    "fwf",     "readr::read_fwf",      NA, # TODO: a writer here!
+    "log",     "readr::read_log",      NA, # TODO: a writer here!
+    "rds",     "readr::read_rds",      "readr::write_rds",
+    "txt",     "readr::read_file",     "readr::write_file",
+    "raw",     "readr::read_file_raw",  NA, # TODO: a writer here!
+    "ssv",     "readr::read_table",    NA, # Space separated values
+    "ssv2",    "readr::read_table2",   NA,
+    "csv.gz",  "readr::read_csv",      "readr::write_csv",
+    "csv2.gz", "readr::read_csv2",     NA,
+    "tsv.gz",  "readr::read_tsv",      "readr::write_tsv",
+    "txt.gz",  "readr::read_file",     "readr::write_file",
+    "csv.bz2", "readr::read_csv",      "readr::write_csv",
+    "csv2.bz2","readr::read_csv2",     NA,
+    "tsv.bz2", "readr::read_tsv",      "readr::write_tsv",
+    "txt.bz2", "readr::read_file",     "readr::write_file",
+    "csv.xz",  "readr::read_csv",      "readr::write_csv",
+    "csv2.xz", "readr::read_csv2",     NA,
+    "tsv.xz",  "readr::read_tsv",      "readr::write_tsv",
+    "txt.xz",  "readr::read_file",     "readr::write_file",
     # This is buggy right now!! "csvy",    "csvy::read_csvy",    "csvy::write_csvy",
-    "xls",     "readxl::read_excel", "WriteXLS::WriteXLS",
-    "xlsx",    "readxl::read_excel", "openxlsx::write.xlsx",
-    "dta",     "haven::read_dta",    "haven::write_dta",
-    "por",     "haven::read_por",    "haven::write_por",
-    "sas",     "haven::read_sas",    "haven::write_sas",
-    "sav",     "haven::read_sav",    "haven::write_sav",
-    "spss",    "haven:read_spss",    NA,
-    "stata",   "haven:read_stata",   NA,
-    "xpt",     "haven::read_xpt",    "haven::write_xpt",
+    "xls",     "readxl::read_excel",   "WriteXLS::WriteXLS",
+    "xlsx",    "readxl::read_excel",   "openxlsx::write.xlsx",
+    "dta",     "haven::read_dta",      "haven::write_dta", # = read_stata()
+    "sas",     "haven::read_sas",      "haven::write_sas",
+    "sas7bdat","haven::read_sas",      "haven::write_sas",
+    "sav",     "haven::read_sav",      "haven::write_sav", # = read_spss()
+    "por",     "haven::read_por",      NA, # = read_spss()
+    "xpt",     "haven::read_xpt",      "haven::write_xpt",
     "feather", "feather::read_feather", "feather::write_feather"
   ))
 }
 
-# TODO: for xls/xlsx, check Openxlsx::write.xlsx, XLConnect
 # TODO: check import and rio
 # TODO: there is a xlsx writer in writexl, but does not support sheets!
 # Note: csvy is csv with YAML header. It offers more configuration (read_csvy, write_csvy)
@@ -190,9 +317,6 @@ type_from_extension <- function(file) {
 #library(haven)
 #library(feather)
 #library(csvy)
-
 #.import.rio_png <- function(file, native = FALSE, info = FALSE, ...) {
 #  readPNG(file, native = native, info = info)
 #}
-
-# Make write an S3 method and make the base::write() function its default one
